8ab44d603a2c850b97c1e614c023b269c1989a35
Élenfe Gomes da Silva <elenfe.silva@medbarra.com.br>
2018-12-21 18:28:04 -0200

    primeiro publish

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <div>
+      <b-card-group deck class="mb-3">
+        <b-card header="FILTRO" class="text-center"></b-card>
+      </b-card-group>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss">
+  @import '../assets/styles/variables';
+  @import '../assets/styles/bootstrap';
+</style>
