53a5fc22cd50c2456fd7156a82dca646c0a99676
Élenfe Gomes da Silva <elenfe.silva@medbarra.com.br>
2018-12-26 19:42:43 -0200

    modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="home">
-    <b-container>
-        <b-row>
+    <b-container fluid>
+        <b-row class="mb-4 mt-4 text-center">
+        <b-col cols="4">FILTRO</b-col>
         <b-col cols="4"></b-col>
-        <b-col cols="4"></b-col>
-        <b-col cols="4"><b-button>CAdastrar concurso</b-button></b-col>
+        <b-btn cols="4" @click="show=true" variant="primary">Cadastrar concurso</b-btn>
       </b-row>
     </b-container>
     <b-container>
         <b-row>
-        <b-col cols="4">
-          <b-form inline>
+        <b-col cols="4" sm="2">
+          <b-form>
             <!-- <b-col cols="4"> -->
           <b-form-select
-                          :value="null"
-                          :options="anos"
-                          required
-                          v-model="form.anos">
+            :value="null"
+            :options="anos"
+            required
+            v-model="form.anos">
           </b-form-select>
           <!-- </b-col> -->
           </b-form>
-          </b-col>
-        <b-col cols="4">
+        </b-col>
+        <b-col cols="4" sm="6">
           <b-form>
             <!-- <b-col cols="4"> -->
           <b-form-select
@@ -47,36 +47,86 @@
           </b-col>
       </b-row>
     </b-container>
-<div class="aprovacoes__box">
-    <div class="lista-aprovados">
-        <h2 class="lista-aprovados__titulo"><span>HMAG ES - 2018<br>Hospital e Maternidade DR. Arthur Gerhardt</span><span class="destaque">Residência Médica</span></h2>
-        <h3 class="lista-aprovados__subtitulo"><span>Resultado Final</span></h3>
-        <h2 class="lista-aprovados__negrito"><span>Aprovados em negrito</span></h2>
-        <div class="content-lista-aprovados">
-            <div class="content-lista-aprovados__container">
-                <div class="content-lista-aprovados__hospital"><span>hospital e maternidade dr. arthur gerhardt</span></div>
-                <div>
-                    <h4 class="content-lista-aprovados__titulo"><span>neonatologia</span></h4>
-                    <div class="content-lista-aprovados__info"><span class="destaque">TODOS</span> os aprovados foram do <span class="destaque">MEDGRUPO</span> !!!</div>
-                    <ul class="content-lista-aprovados__nomes stamp">
-                        <li class=""><span class="destaque">1. juliana neves ferreira</span><span>MEDGRUPO</span></li>
-                        <li class=""><span class="destaque">2. sara dos santos jorge</span><span>MEDGRUPO</span></li>
-                        <li class=""><span class="">3. janine horsth silva</span><span>MEDGRUPO</span></li>
-                        <li class=""><span class="">4. adanna débora brunou capila</span><span>MEDGRUPO</span></li>
-                        <li class=""><span class="">5. lara nicoli passamani</span><span>MEDGRUPO</span></li>
-                        <li class=""><span class="">6. letícia altoé almeida e silva</span><span>MEDGRUPO</span></li>
-                        <li class=""><span class="">7. amanda neves siqueira</span><span>MEDGRUPO</span></li>
-                        <li class=""><span class="">8. renata cristina moreira queiroz</span><span>MEDGRUPO</span></li>
-                        <li class=""><span class="">9. rainara fratus avance baggieri</span><span>MEDGRUPO</span></li>
-                        <li class=""><span class="">10. marcela assis freitas</span><span>MEDGRUPO</span></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    <template>
 
-
+    </template>
+<!-- Cadastro de aprovações -->
+<template>
+  <div>
+    <b-modal v-model="show" size="lg">
+       <b-container>
+         <b-row class="mb-3">
+           <b-col cols="3"><b-form-select
+                          :value="null"
+                          :options="anos"
+                          required
+                          v-model="form.anos" /></b-col>
+           <b-col><b-form-select
+                          :value="null"
+                          :options="concurso"
+                          required
+                          v-model="form.concurso" /></b-col>
+           <b-col><b-form-select
+                          :value="null"
+                          :options="sigla"
+                          required
+                          v-model="form.sigla" /></b-col>
+         </b-row>
+         <b-row class="mb-3">
+           <b-col cols="2"><b-form-select
+                          :value="null"
+                          :options="especialidade"
+                          required
+                          v-model="form.especialidade" /></b-col>
+           <b-col><b-form-select
+                          :value="null"
+                          :options="hospital"
+                          required
+                          v-model="form.hospital" /></b-col>
+         </b-row>
+         <b-row class="mb-3">
+           <b-col cols="12" ><b-form-textarea id="textarea3"
+                   v-model="text1"
+                   placeholder="Digite o Texto"
+                   :rows="3"></b-form-textarea></b-col>
+          </b-row>
+        <b-row class="mb-2">
+            <b-col sm="2" cols="4">
+              <b-form-select
+                        :options="posicao"
+                        required />
+            </b-col>
+            <b-col sm="6">
+              <b-form-group horizontal
+                :label-cols="1"
+                label-for="input_default">
+                <b-form-input id="input_default"></b-form-input>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group>
+                  <b-form-checkbox-group plain stacked v-model="selected" :options="options" />
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-btn size="sm" class="float-left" variant="primary">
+                +
+              </b-btn>
+            </b-col>
+        </b-row>
+       </b-container>
+       <div slot="modal-footer" class="w-100">
+         <b-btn size="sm" class="float-right" variant="primary" @click="show=false">
+           Salvar
+         </b-btn>
+         <b-btn size="sm" class="float-left" variant="primary" @click="show=false">
+           Fechar
+         </b-btn>
+       </div>
+    </b-modal>
+  </div>
+</template>
+<!-- Fim cadastro de aprovações -->
 </div>
 
 </template>
@@ -96,7 +146,11 @@ export default class Home extends Vue {
       form: {
         anos: null,
         concurso: null,
-        sigla: null
+        sigla: null,
+        especialidade: null,
+        hospital: null,
+        texto: null,
+        nomeAluno: null
       },
       anos: [
         { text: 'Selecione o Ano', value: null },
@@ -109,7 +163,24 @@ export default class Home extends Vue {
        sigla: [
         { text: 'Sigla', value: null },
         'HC-RJ', 'HO-RJ', 'HSF-RJ', 'HLJ-RJ'
+      ],
+       especialidade: [
+        { text: 'Especialidade', value: null },
+        'Cirurgia', 'ginecologia', 'Clinica Medica', 'Ortopedia'
+      ],
+       hospital: [
+        { text: 'Hospital', value: null },
+        'Hospital Municipal Salgado Filho', 'Hospital Municipal lourenço Jorge', 'Hospital Estadua do Carmo', 'Hospital Municipal Suza Aguiar'
+      ],
+       text: '',
+       text1: '',
+      show: true,
+      selected: [],
+      options: [
+        {text: 'MEDGRUPO', value: 'medgrupo'},
+        {text: 'SUPLENTE', value: 'suplente'},
       ]
+
     }
   }
 }
